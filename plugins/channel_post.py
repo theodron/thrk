f14d2da549ca1c1f57aaeffd172a9a84594423b9
Sukuna69x <niloygain29@gmail.com>
2025-02-04 14:11:53 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,15 +1,34 @@
 import asyncio
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import FloodWait
+from pyrogram.errors import FloodWait, UserNotParticipant
 
 from bot import Bot
-from config import CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+from config import CHANNEL_ID, DISABLE_CHANNEL_BUTTON, FORCE_SUB_CHANNEL_1, FORCE_SUB_CHANNEL_2
 from helper_func import encode
+
+async def is_user_member(client: Client, user_id: int, channel_id: int) -> bool:
+    try:
+        member = await client.get_chat_member(channel_id, user_id)
+        return member.status in ["member", "administrator", "creator"]
+    except UserNotParticipant:
+        return False
 
 @Bot.on_message(filters.private & ~filters.command(['start', 'users', 'broadcast', 'batch', 'genlink', 'stats']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Please Wait...!", quote=True)
+
+    # Check if the user is an admin or a member of the required channels
+    is_admin = message.from_user.id in [admin.user.id for admin in await client.get_chat_administrators(client.db_channel.id)]
+    if not is_admin:
+        # Check membership in both channels
+        is_member_1 = await is_user_member(client, message.from_user.id, FORCE_SUB_CHANNEL_1)
+        is_member_2 = await is_user_member(client, message.from_user.id, FORCE_SUB_CHANNEL_2)
+
+        if not (is_member_1 and is_member_2):
+            await reply_text.edit_text("You must join the required channels to generate a link.")
+            return
+
     try:
         post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except FloodWait as e:
